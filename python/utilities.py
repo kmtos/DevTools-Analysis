2de49f28f4fb88a37d01b2ebd7b3abc5aa4944f5
Devin Taylor <dntaylor@wisc.edu>
2016-04-23 10:20:18 -0500

    update to latest ntuples

--- python/utilities.py
+++ python/utilities.py
@@ -19,7 +19,7 @@ def deltaR(eta0,phi0,eta1,phi1):
     return ROOT.TMath.Sqrt(deta**2+dphi**2)
 
 latestNtuples = {
-    'DevTools' : '2016-03-19_DevTools_76X_v1',
+    'DevTools' : '2016-04-22_DevTools_76X_v1',
 }
 
 def getNtupleDirectory(analysis):
@@ -29,36 +29,15 @@ def getNtupleDirectory(analysis):
 
 def getTestFiles(type):
     if type=='MC':
-        return ['/store/user/dntaylor/2016-03-19_DevTools_76X_v1/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/2016-03-19_DevTools_76X_v1/160319_195847/0000/miniTree_1.root']
+        return ['/store/user/dntaylor/2016-04-22_DevTools_76X_v1/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/2016-04-22_DevTools_76X_v1/160422_113101/0000/miniTree_1.root']
     elif type=='Data':
-        return ['/store/user/dntaylor/2016-03-19_DevTools_76X_v1/DoubleMuon/2016-03-19_DevTools_76X_v1/160319_200028/0000/miniTree_111.root']
+        return ['/store/user/dntaylor/2016-04-22_DevTools_76X_v1/MuonEG/2016-04-22_DevTools_76X_v1/160422_113532/0000/miniTree_1.root']
     elif type=='hpp':
-        return ['/store/user/dntaylor/2016-03-19_DevTools_76X_v1/HPlusPlusHMinusMinusHTo4L_M-500_13TeV-pythia8/2016-03-19_DevTools_76X_v1/160320_073839/0000/miniTree_1.root']
+        return ['/store/user/dntaylor/2016-04-22_DevTools_76X_v1/HPlusPlusHMinusMinusHTo4L_M-500_13TeV-pythia8/2016-04-22_DevTools_76X_v1/160422_113847/0000/miniTree_1.root']
     elif type=='dy':
-        return ['/store/user/dntaylor/2016-03-19_DevTools_76X_v1/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/2016-03-19_DevTools_76X_v1/160319_195502/0001/miniTree_1111.root']
+        return ['/store/user/dntaylor/2016-04-22_DevTools_76X_v1/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/2016-04-22_DevTools_76X_v1/160422_112025/0000/miniTree_1.root']
     elif type=='long':
         return [
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_54.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_55.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_56.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_57.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_58.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_59.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_6.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_60.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_61.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_62.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_63.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_64.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_65.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_66.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_67.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_94.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_95.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_96.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_97.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_98.root',
-            '/store/user/dntaylor/2016-03-19_DevTools_76X_v1/ZZTo4L_13TeV_powheg_pythia8/2016-03-19_DevTools_76X_v1/160319_195936/0000/miniTree_99.root',
         ]
     else:
         return ['']
