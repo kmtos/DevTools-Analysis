d80ea0df9abad38794fc19c17423e62abfaadc62
Devin <dntaylor@wisc.edu>
2017-07-06 14:38:30 -0500

    fixed file acces

--- python/utilities.py
+++ python/utilities.py
@@ -60,8 +60,6 @@ def getTestFiles(sample,n=1,version=None):
     
     files = get_hdfs_root_files('{0}/{1}'.format(getNtupleDirectory(version=version),sampleMap[sample]))
 
-    #files = [f.replace('/hdfs','') for f in glob.glob('{0}/{1}/*/*/*/*.root'.format(getNtupleDirectory(version=version),sampleMap[sample]))]
-
     if sample=='wz': return files[1:min(n+1,len(files)-1)] # temporary hack to get a better WZ sample (Summer16 MC)
     return files[:min(n,len(files))]
 
